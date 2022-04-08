3371df6715bc68a3ec6d019a321e4ef5a0a4e61c
edpo1998 <edpo11081998@gmail.com>
2022-04-07 23:00:27 -0600

    add redis

--- bases/redis.go
+++ bases/redis.go
@@ -1,30 +1,98 @@
+//Para test
+//package main
 package bases
 
 import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/go-redis/redis"
+	redis "github.com/garyburd/redigo/redis"
 )
 
-var ADDRREDIS = ""
-var PASSWORDREDIS = ""
-
-func SaveLogRedis(bjsonLog []byte) {
-	fmt.Println("Go Redis Tutorial")
-	client := redis.NewClient(&redis.Options{
-		Addr:     ADDRREDIS,
-		Password: PASSWORDREDIS,
-		DB:       0,
-	})
-
-	json, err := json.Marshal(bjsonLog)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = client.Set("id1234", json, 0).Err()
-	if err != nil {
-		fmt.Println(err)
-	}
+type Game struct {
+	Nombre_Juego   string `json:"nombre_juego"`
+	Nombre_Ganador string `json:"nombre_ganador"`
 }
+
+var ADDRREDIS = "35.206.76.88"
+var PASSWORDREDIS = "1234"
+var PORTREDIS = "6379"
+
+/*
+// Para Test
+func main() {
+	saveDataRedisStr("Juego_Random", "25")
+}
+*/
+
+func saveDataRedisObj(game *Game) {
+	// Establecer conexion
+	c, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", ADDRREDIS, PORTREDIS))
+
+	if err != nil {
+		fmt.Println("Not found Database 404")
+	}
+
+	// Authenticacion
+	_, err = c.Do("AUTH", "1234")
+
+	defer c.Close()
+
+	json, err := json.Marshal(game)
+
+	if err != nil {
+		fmt.Println("No Generate JSON")
+	}
+
+	_, err = c.Do("LPUSH", "gamesobj", json)
+	if err != nil {
+		fmt.Println("Error Insert Data")
+	} else {
+		fmt.Println("Data Inserted Redis")
+	}
+	/*
+		// See Data
+		val, err := redis.Strings(c.Do("LRANGE", "gamesobj", 0, -1))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(val)
+	*/
+}
+
+func saveDataRedisStr(game, winner string) {
+
+	c, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", ADDRREDIS, PORTREDIS))
+
+	if err != nil {
+		fmt.Println("Not found Database 404")
+	}
+
+	_, autherror := c.Do("AUTH", "1234")
+
+	if autherror != nil {
+		fmt.Println("Error authentication")
+	}
+
+	defer c.Close()
+
+	_, err = c.Do("LPUSH", "gamesstr", game, winner)
+	if err != nil {
+		fmt.Println("Error Insert Data")
+	} else {
+		fmt.Println("Data Inserted Redis")
+	}
+	/*
+		// See Data
+		val, err := redis.Strings(c.Do("LRANGE", "gamesstr", 0, -1))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(val)
+	*/
+
+}
+
